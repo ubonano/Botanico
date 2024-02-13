db4f14f47ed6ec11adc40db39498319f770ec3ab
Uriel Bonano <ubonano@gmail.com>
2024-02-12 23:22:07 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() => runApp(const MyApp());
+import 'auth/controllers/auth_controller.dart';
+import 'auth/pages/login_page.dart';
+import 'config/bindings.dart';
+import 'config/firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
+  Get.put(AuthController()); // TODO Debe ir en AppBindings
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Botanico',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Botanico V 0'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+    return GetMaterialApp(
+      initialBinding: AppBindings(),
+      home: LoginPage(),
     );
   }
 }
