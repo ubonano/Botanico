5d0fc64c4bd43ac9ffaa08c61336bf52d40c8820
Uriel Bonano <ubonano@gmail.com>
2024-02-16 22:12:12 -0300

    not auth middleware

--- lib/config/pages.dart
+++ lib/config/pages.dart
@@ -3,6 +3,7 @@ import 'package:botanico/auth/pages/user_profile_form_page.dart';
 import 'package:get/get.dart';
 
 import '../auth/middlewares/auth_middleware.dart';
+import '../auth/middlewares/not_auth_middleware.dart';
 import '../auth/pages/login_page.dart';
 import '../auth/pages/sign_up_page.dart';
 import '../pages/home_page.dart';
@@ -21,10 +22,16 @@ class Pages {
     GetPage(
       name: Routes.LOGIN,
       page: () => const LoginPage(),
+      middlewares: [
+        NotAuthMiddleware(),
+      ],
     ),
     GetPage(
       name: Routes.SIGN_UP,
       page: () => const SignUpPage(),
+      middlewares: [
+        NotAuthMiddleware(),
+      ],
     ),
     GetPage(
       name: Routes.USER_PROFILE_FORM,
