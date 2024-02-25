d3369721f33b6583f92d5087838f402dca34e197
Uriel Bonano <ubonano@gmail.com>
2024-02-24 21:50:47 -0300

    company model

---
+++ lib/modules/foundation/models/company_model.dart
@@ -0,0 +1,39 @@
+class Company {
+  final String name;
+  final String address;
+  final String city;
+  final String province;
+  final String country;
+  final String phone;
+
+  Company({
+    required this.name,
+    required this.address,
+    required this.city,
+    required this.province,
+    required this.country,
+    required this.phone,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'address': address,
+      'city': city,
+      'province': province,
+      'country': country,
+      'phone': phone,
+    };
+  }
+
+  factory Company.fromMap(Map<String, dynamic> map) {
+    return Company(
+      name: map['name'],
+      address: map['address'],
+      city: map['city'],
+      province: map['province'],
+      country: map['country'],
+      phone: map['phone'],
+    );
+  }
+}
