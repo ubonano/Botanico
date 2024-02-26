25d367da1686d69f542d332adc140b8c19d7daba
Uriel Bonano <ubonano@gmail.com>
2024-02-25 21:04:50 -0300

    model refactor

--- lib/modules/user_profile/user_profile_model.dart
+++ lib/modules/user_profile/user_profile_model.dart
@@ -17,6 +17,26 @@ class UserProfileModel {
     this.companyUid = '',
   });
 
+  UserProfileModel copyWith({
+    String? uid,
+    String? email,
+    String? name,
+    String? birthDate,
+    String? phone,
+    String? dni,
+    String? companyUid,
+  }) {
+    return UserProfileModel(
+      uid: uid ?? this.uid,
+      email: email ?? this.email,
+      name: name ?? this.name,
+      birthDate: birthDate ?? this.birthDate,
+      phone: phone ?? this.phone,
+      dni: dni ?? this.dni,
+      companyUid: companyUid ?? this.companyUid,
+    );
+  }
+
   Map<String, dynamic> toMap() {
     return {
       'uid': uid,
