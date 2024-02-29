0be0157a01288c5a11fc585035454307dbd3b789
Uriel Bonano <ubonano@gmail.com>
2024-02-29 00:54:06 -0300

    pages refactor

--- lib/modules/foundation/config/pages.dart
+++ lib/modules/foundation/config/pages.dart
@@ -1,14 +1,10 @@
-import 'package:botanico/modules/auth/auth_middleware.dart';
 import 'package:botanico/modules/user_profile/user_profile_page.dart';
 import 'package:get/get.dart';
-
 import '../../auth/sign_in/sign_in_page.dart';
 import '../../auth/sign_up/sign_up_page.dart';
 import '../../company_profile/company_profile_page.dart';
-import '../../home_page.dart';
+import '../../home/home_page.dart';
 import '../../lobby/lobby_page.dart';
-import '../../company_profile/company_profile_middleware.dart';
-import '../../user_profile/user_profile_middleware.dart';
 import 'routes.dart';
 
 class Pages {
@@ -16,48 +12,26 @@ class Pages {
     GetPage(
       name: Routes.HOME,
       page: () => const HomePage(),
-      middlewares: [
-        AuthMiddleware(),
-        UserProfileMiddleware(),
-        CompanyProfileMiddleware(),
-      ],
     ),
     GetPage(
       name: Routes.LOBBY,
       page: () => const LobbyPage(),
-      middlewares: [
-        AuthMiddleware(),
-        UserProfileMiddleware(),
-      ],
     ),
     GetPage(
       name: Routes.SIGN_IN,
-      page: () => SignInPage(),
-      middlewares: [
-        AuthMiddleware(),
-      ],
+      page: () => const SignInPage(),
     ),
     GetPage(
       name: Routes.SIGN_UP,
-      page: () => SignUpPage(),
-      middlewares: [
-        AuthMiddleware(),
-      ],
+      page: () => const SignUpPage(),
     ),
     GetPage(
       name: Routes.USER_PROFILE,
       page: () => UserProfilePage(),
-      middlewares: [
-        AuthMiddleware(),
-      ],
     ),
     GetPage(
       name: Routes.COMPANY_PROFILE,
-      page: () => CompanyProfilePage(),
-      middlewares: [
-        AuthMiddleware(),
-        UserProfileMiddleware(),
-      ],
+      page: () => const CompanyProfilePage(),
     ),
   ];
 }
