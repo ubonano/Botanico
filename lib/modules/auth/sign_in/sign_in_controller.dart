7852f0072ec827570ee5c88f682e6d2e5b993668
Uriel Bonano <ubonano@gmail.com>
2024-02-28 22:11:52 -0300

    sign in refactor

--- lib/modules/auth/sign_in/sign_in_controller.dart
+++ lib/modules/auth/sign_in/sign_in_controller.dart
@@ -7,6 +7,9 @@ class SignInController extends GetxController with CommonServices, LogLifecycleC
   @override
   String get logTag => 'SignInController';
 
+  final formKey = GlobalKey<FormState>();
+  final recoverPasswordFormKey = GlobalKey<FormState>();
+
   final emailController = TextEditingController();
   final emailRecoverController = TextEditingController();
   final passwordController = TextEditingController();
@@ -15,32 +18,42 @@ class SignInController extends GetxController with CommonServices, LogLifecycleC
   String get _emailRecover => emailRecoverController.text.trim();
   String get _password => passwordController.text.trim();
 
-  Future<void> signIn() async => await asyncOperationService.performOperation(
-        operation: () => authService.signInWithEmailAndPassword(_email, _password),
-        operationName: 'Sign in',
-        onSuccess: () async {
-          await userProfileService.fetchUserProfile(authService.currentUser!.uid);
-          await companyProfileService.fetchCompanyProfile(authService.currentUser!.uid);
+  Future<void> signIn() async {
+    if (!formKey.currentState!.validate()) return;
 
-          if (!userProfileService.hasUserProfile) {
-            navigationService.toUserProfile();
-          }
+    await asyncOperationService.performOperation(
+      operationName: 'Sign in',
+      operation: () => authService.signInWithEmailAndPassword(_email, _password),
+      onSuccess: () async {
+        await fetchUserProfile();
+        await fetchCompanyProfile();
 
-          if (userProfileService.hasUserProfile && !companyProfileService.hasCompanyProfile) {
-            navigationService.toLobby();
-          }
+        if (userProfileService.hasUserProfile && companyProfileService.hasCompanyProfile) {
+          navigationService.toHome();
+        }
 
-          if (userProfileService.hasUserProfile && companyProfileService.hasCompanyProfile) {
-            navigationService.toHome();
-          }
-        },
-      );
+        if (userProfileService.hasUserProfile && !companyProfileService.hasCompanyProfile) {
+          navigationService.toLobby();
+        }
+
+        if (!userProfileService.hasUserProfile) {
+          navigationService.toUserProfile();
+        }
+      },
+    );
+  }
+
+  Future<void> fetchUserProfile() async => await userProfileService.fetchById(authService.user!.uid);
+  Future<void> fetchCompanyProfile() async =>
+      await companyProfileService.fetchById(userProfileService.userProfile!.companyUid);
 
   void recoverPassword() async {
+    if (!recoverPasswordFormKey.currentState!.validate()) return;
+
     await asyncOperationService.performOperation(
-      operation: () => authService.recoverPassword(_emailRecover),
-      operationName: 'Recover password',
-    );
+        operation: () => authService.recoverPassword(_emailRecover),
+        operationName: 'Recover password',
+        successMessage: 'Se envio un email a tu casilla para restaurar tu contraseña');
 
     navigationService.back();
   }
