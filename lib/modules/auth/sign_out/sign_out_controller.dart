d75b3c5ae4afe337f77a2531c58f0bbc1eb2f74b
Uriel Bonano <ubonano@gmail.com>
2024-03-03 04:01:53 -0300

    sign out controller refactor

--- lib/modules/auth/sign_out/sign_out_controller.dart
+++ lib/modules/auth/sign_out/sign_out_controller.dart
@@ -1,17 +1,15 @@
 import 'package:get/get.dart';
-import '../../foundation/utils/log_lifecycle.dart';
+import '../../foundation/utils/custom_controller.dart';
 
 class SignOutController extends GetxController with CustomController {
   @override
   String get logTag => 'SignOutController';
 
-  Future<void> signOut() async => asyncOperation.perform(
-        operationName: 'Sign out',
-        operation: auth.signOut,
-        onSuccess: () {
-          userProfileService.clean();
-          companyProfileService.clean();
-          navigate.toSignIn();
-        },
-      );
+  Future<void> signOut() async => async.perform(
+      operationName: 'Sign out',
+      operation: () async {
+        await auth.signOut();
+        cleanData();
+      },
+      onSuccess: navigate.toSignIn);
 }
