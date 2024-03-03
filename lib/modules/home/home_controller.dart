1b8ce6ea576cfd14eac7110effeaf2921990b213
Uriel Bonano <ubonano@gmail.com>
2024-03-03 05:06:36 -0300

    home refactor

--- lib/modules/home/home_controller.dart
+++ lib/modules/home/home_controller.dart
@@ -4,9 +4,4 @@ import '../foundation/utils/custom_controller.dart';
 class HomeController extends GetxController with CustomController {
   @override
   String get logTag => 'HomeController';
-
-  @override
-  void onInit() {
-    super.onInit();
-  }
 }
