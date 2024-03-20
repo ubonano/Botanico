a0ca47345964c20855df7cb19a39881a00a2f879
Uriel Bonano <ubonano@gmail.com>
2024-03-19 22:22:22 -0300

    documentation services

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,39 +1,55 @@
-// Import necessary packages
 import 'dart:async';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import '../utils/custom_service.dart';
 
-// Define AuthService class which extends GetxService for dependency management
-// and includes CustomService for custom service methods
+/// A service to manage user authentication.
+///
+/// This class provides methods to handle signing in, signing up, signing out,
+/// and password recovery using Firebase Authentication.
 class AuthService extends GetxService with CustomService {
-  // Set a logTag string for identifying this service in logs
   @override
   String get logTag => 'AuthService';
 
-  // Instance of FirebaseAuth to do all Firebase authentication related operations
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // Stream that emits changes in the user authentication status
+  /// Stream of authentication state changes.
+  ///
+  /// Listens for changes in the user's sign-in state (e.g., user signs in or out).
   Stream<User?> get userChanges => _auth.authStateChanges();
 
-  // Property to get the authentication status of the current user
+  /// Gets the current user.
+  ///
+  /// Returns the currently signed-in [User], or null if there is none.
   User? get user => _auth.currentUser;
 
-  // Method to sign in a user with email and password. It returns a Future that resolves to User object on successful sign in and null if not.
+  /// Signs in a user with an email and password.
+  ///
+  /// Attempts to sign in a user with the given [email] and [password].
+  /// Returns the [User] on successful sign-in.
+  ///
+  /// Throws a [FirebaseAuthException] if there is an error.
   Future<User?> signIn(String email, String password) async {
     return (await _auth.signInWithEmailAndPassword(email: email, password: password)).user;
   }
 
-  // Method to sign up a user with email and password. It returns a Future that
-  // resolves to User object on successful sign up and null if sign up fails.
+  /// Signs up a user with an email and password.
+  ///
+  /// Attempts to create a new user with the given [email] and [password].
+  /// Returns the newly created [User] on successful sign-up.
+  ///
+  /// Throws a [FirebaseAuthException] if there is an error.
   Future<User?> signUp(String email, String password) async {
     return (await _auth.createUserWithEmailAndPassword(email: email, password: password)).user;
   }
 
-  // Method to sign out the currently authenticated user
+  /// Signs out the current user.
+  ///
+  /// Signs out the currently signed-in user and clears their session.
   Future<void> signOut() async => await _auth.signOut();
 
-  // Method to recover password using email. Firebase sends a password reset email to the user
+  /// Sends a password reset email to the given email address.
+  ///
+  /// Initiates a password reset for the user associated with the specified [email].
   Future<void> recoverPassword(String email) async => await _auth.sendPasswordResetEmail(email: email);
 }
