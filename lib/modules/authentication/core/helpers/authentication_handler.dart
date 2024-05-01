eaeb71280d46f95c41dc06d60337a869a56c9215
Uriel Bonano <ubonano@gmail.com>
2024-05-01 01:01:44 -0300

    auth refactor

--- lib/modules/authentication/core/helpers/authentication_handler.dart
+++ lib/modules/authentication/core/helpers/authentication_handler.dart
@@ -5,20 +5,19 @@ import 'package:botanico/modules/worker/worker_module.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class AuthenticationHandler with GlobalServices, CompanyContext {
+class AuthenticationHandler with GlobalServices {
   late final AuthenticationRepository _authRepo = Get.find();
+
+  late final CompanyHandler _companyHandler = Get.find();
   late final WorkerHandler _workerHandler = Get.find();
 
   User? get currentUser => _authRepo.currentUser;
-
   bool get isLoggedInUser => _authRepo.currentUser != null;
 
   Future<User?> signUp(String email, String password) async => await _authRepo.signUp(email, password);
-
   void postSignUp() async => navigate.toWorkerCreate();
 
   Future<void> signIn(String email, String password) async => await _authRepo.signIn(email, password);
-
   Future<void> postSignIn() async {
     WorkerModel? worker = await _workerHandler.fetchLoggedWorker();
 
@@ -27,7 +26,7 @@ class AuthenticationHandler with GlobalServices, CompanyContext {
       return;
     }
 
-    CompanyModel? company = await companyRepo.fetch(worker.companyId);
+    CompanyModel? company = await _companyHandler.fetch(worker.companyId);
     if (company != null) {
       navigate.toHome();
     } else {
@@ -36,10 +35,9 @@ class AuthenticationHandler with GlobalServices, CompanyContext {
   }
 
   Future<void> signOut() async => await _authRepo.signOut();
-
   void postSignOut() async {
     _workerHandler.clearCurrentWorker();
-    companyRepo.clearCurrentCompany();
+    _companyHandler.clearCurrentCompany();
 
     navigate.toSignIn();
   }
