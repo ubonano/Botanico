d4c626e377e3075f092069847f5edc2b12570478
Uriel Bonano <ubonano@gmail.com>
2024-05-02 22:50:44 -0300

    foundation refactor

--- lib/modules/foundation/foundation_module.dart
+++ lib/modules/foundation/foundation_module.dart
@@ -32,20 +32,8 @@ export 'core/helpers/navigation_helper.dart';
 
 export 'ui/widgets/buttons/custom_button.dart';
 export 'ui/widgets/buttons/custom_text_button.dart';
-export 'ui/widgets/input_fields/address_input_field.dart';
-export 'ui/widgets/input_fields/birthdate_input_field.dart';
-export 'ui/widgets/input_fields/city_input_field.dart';
-export 'ui/widgets/input_fields/confirm_password_input_field.dart';
-export 'ui/widgets/input_fields/country_input_field.dart';
-export 'ui/widgets/input_fields/custom_input_field.dart';
-export 'ui/widgets/input_fields/dni_input_field.dart';
-export 'ui/widgets/input_fields/email_input_field.dart';
-export 'ui/widgets/input_fields/fullname_input_field.dart';
-export 'ui/widgets/input_fields/name_input_field.dart';
-export 'ui/widgets/input_fields/password_input_field.dart';
-export 'ui/widgets/input_fields/phone_input_field.dart';
-export 'ui/widgets/input_fields/province_input_field.dart';
-export 'ui/widgets/input_fields/worker_uid_input_field.dart';
+export 'ui/widgets/custom_input_field.dart';
+
 export 'ui/widgets/module_expansion_tile.dart';
 export 'ui/widgets/custom_drawer.dart';
 export 'ui/widgets/confirmation_dialog.dart';
