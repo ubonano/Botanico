6db85ef1251972a275d5e9352735aaf8dcf2f67a
Uriel Bonano <ubonano@gmail.com>
2024-06-03 21:24:23 -0300

    prepare deploy to QA

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:botanico/modules/foundation/module.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import 'setup/firebase_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // firebase deploy --only hosting:qa,firestore,functions,storage --project botanico-qa
+  // firebase deploy --only hosting:prod,firestore,functions,storage --project botanico-82e04
 
-  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+
+  // const String environment = String.fromEnvironment('ENV', defaultValue: 'prod');
+  const String environment = String.fromEnvironment('ENV', defaultValue: 'qa');
+
+  await FirebaseService.initializeFirebase(environment);
+
+  // FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   runApp(const App());
 }
---
+++ lib/setup/firebase_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseService {
+  static Future<void> initializeFirebase(String environment) async {
+    String configPath = 'lib/setup/firebase_config_$environment.json';
+    final configString = await rootBundle.loadString(configPath);
+    final config = json.decode(configString);
+
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: config['apiKey'],
+        authDomain: config['authDomain'],
+        projectId: config['projectId'],
+        storageBucket: config['storageBucket'],
+        messagingSenderId: config['messagingSenderId'],
+        appId: config['appId'],
+      ),
+    );
+  }
+}
