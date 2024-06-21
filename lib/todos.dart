32c9f194970b238849b7cf53113e72729c6ddad8
Uriel Bonano <ubonano@gmail.com>
2024-06-20 22:37:51 -0300

    delete pending field from form

--- lib/todos.dart
+++ lib/todos.dart
@@ -2,7 +2,6 @@
 
 // TODO Fixear problema al buscar envio por ID, si falla queda el loading del boton
 
-//Que se copie el nro de guia al presionar en el
 // El campo ESTADO no es necesario acá en este form, por defecto tiene que ser pendiente siempre (creacion y actualizcion de envios)
 // y el campo lugar de entrega esta como obligatorio, tiene que ser opcional, mayormente eso se decide despues de crear el item
 // lo mismo con el tipo: por defecto ENTREGA
