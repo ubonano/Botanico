bc203f37fb48f7e44c5fd6ddf3544f431ddad61d
Uriel Bonano <ubonano@gmail.com>
2024-06-21 01:01:42 -0300

    fix

--- lib/todos.dart
+++ lib/todos.dart
@@ -2,9 +2,6 @@
 
 
 
-// al actualizar la empreda, esta pisando la lista de modulos
-
-
 
 // Bloquear la odificacion del la activacion del modulo Empresa
 
@@ -25,20 +22,3 @@
 // TODO refactorizar modulo de WORKER (en general) Inclusive hay que agregar el paginationHelper
 
 // TODO crear un servicio para el porta papeles y que dentro tambien tenga la posiblidad de mostrr snackbakrs si uno quisiera advertir al usuario que se copio algo, por ejemplo
-
-
-
-
-/**
- * 
- * Se modifco:
- *  Colores de los botones.
- *  Modulos protegidos, los usuarios no deben ver los modulos que no estan activados, y los que no tiene permisos.
- *  Formulario de creacion/actualizacion en formato columna
- *  Copiar numero de guia al precionar sobre el mismo
- *  Alerta de pendiente en color naranja. (Se mantiene en rojo las que fueron facturadas y no estan listas/entregadas)
- *  Cambio de orden de las guias, Ahora se muestran las mas nuevas ultimas.
- *  Se quito del formulario el campo estado, lugar de entrega y facturado.
- *  Se arreglo el problema de que al intentar buscar un envio mediante la api, en el caso de que fallara, el boton quedaba inaccesible (quedaba el loading eternamente)
- *   
- */
