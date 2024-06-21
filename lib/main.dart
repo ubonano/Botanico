d32e7126e2412b9e6adaa223ae5f513bcd2e8b2e
Uriel Bonano <ubonano@gmail.com>
2024-06-20 22:12:42 -0300

    button colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:botanico/modules/foundation/module.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import 'setup/firebase_service.dart';
@@ -11,14 +11,13 @@ Future<void> main() async {
   // firebase deploy --only hosting:qa,firestore,functions,storage --project botanico-qa
   // firebase deploy --only hosting:prod,firestore,functions,storage --project botanico-82e04
 
-
   // const String environment = String.fromEnvironment('ENV', defaultValue: 'prod');
   const String environment = String.fromEnvironment('ENV', defaultValue: 'qa');
 
   await FirebaseService.initializeFirebase(environment);
 
-  // FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
-  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   runApp(const App());
 }
--- lib/todos.dart
+++ lib/todos.dart
@@ -1,8 +1,21 @@
 // CREa modulo de COMPRAS! Finaliza el feature y comenza uno nuevo
 
-// TODO Ver como hacer para que luego de 5 minutos de que una orden en estado entregada se archive sola
-// finalizar feature, hacer release a QA, deployar en QA
+// TODO Fixear problema al buscar envio por ID, si falla queda el loading del boton
+
+// cambiar orden en el dashboard, al revez
+//Que se copie el nro de guia al presionar en el
+// El campo ESTADO no es necesario acá en este form, por defecto tiene que ser pendiente siempre (creacion y actualizcion de envios)
+// y el campo lugar de entrega esta como obligatorio, tiene que ser opcional, mayormente eso se decide despues de crear el item
+// lo mismo con el tipo: por defecto ENTREGA
+
+// al actualizar la empreda, esta pisando la lista de modulos
+
+// Revisar que la sesion se mantenga activa siempre
+
+// Crear proceso programaado para que elimine todos los dias los envios archivados de dos meses hacia atras
+// Refactor de modulos
 // TODO Crear ABM de lugares de entrega
+// TODO Bloquear modulo de trabajadores y empresa
 
 // TODO no funciona la transaccion en operation_helper, si falla la transaccion no hace el rollback
 // TODO, resolver que en los navigation helpers, en todos se agrega el metodo TO... no tienen que aclarase en todos, ver como resolverlo
