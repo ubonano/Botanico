f76f31013a046cb11420f9b16212e73b402286b3
Uriel Bonano <ubonano@gmail.com>
2024-06-26 22:38:40 -0300

    fix

--- lib/modules/shipment/content/setup/navigation.dart
+++ lib/modules/shipment/content/setup/navigation.dart
@@ -5,28 +5,28 @@ import 'package:get/get.dart';
 import '../../ui/ui.dart';
 
 mixin ViaShipmentNavigation {
-  static const VIA_SHIPMENT_FORM = '/via-shipment-form';
-  static const VIA_SHIPMENT_LIST = '/via-shipment-list';
-  static const VIA_SHIPMENT_DASHBOARD = '/via-shipment-dashboard';
+  static const SHIPMENT_FORM = '/shipment-form';
+  static const SHIPMENT_LIST = '/shipment-list';
+  static const SHIPMENT_DASHBOARD = '/shipment-dashboard';
 
-  void toShipmentList({bool canPop = false}) => to(VIA_SHIPMENT_LIST, canPop: canPop);
-  void toShipmentForm({String id = '', bool canPop = false}) => to(VIA_SHIPMENT_FORM, arguments: id, canPop: canPop);
-  void toShipmentDashboard({bool canPop = false}) => to(VIA_SHIPMENT_DASHBOARD, canPop: canPop);
+  void toShipmentList({bool canPop = false}) => to(SHIPMENT_LIST, canPop: canPop);
+  void toShipmentForm({String id = '', bool canPop = false}) => to(SHIPMENT_FORM, arguments: id, canPop: canPop);
+  void toShipmentDashboard({bool canPop = false}) => to(SHIPMENT_DASHBOARD, canPop: canPop);
 
   void to(String route, {dynamic arguments, bool canPop = false});
 }
 
 final pages = [
   GetPage(
-    name: ViaShipmentNavigation.VIA_SHIPMENT_FORM,
+    name: ViaShipmentNavigation.SHIPMENT_FORM,
     page: () => const ShipmentFormPage(),
   ),
   GetPage(
-    name: ViaShipmentNavigation.VIA_SHIPMENT_LIST,
+    name: ViaShipmentNavigation.SHIPMENT_LIST,
     page: () => const ShipmentListPage(),
   ),
   GetPage(
-    name: ViaShipmentNavigation.VIA_SHIPMENT_DASHBOARD,
+    name: ViaShipmentNavigation.SHIPMENT_DASHBOARD,
     page: () => const ShipmentDashboardPage(),
   ),
 ];
